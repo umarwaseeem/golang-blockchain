ed17b39be88697a7028f1bcd4ce3df1489d8e615
Abdul Wahab <awzahid02@gmail.com>
2023-11-26 21:27:26 +0500

    Part 1 done

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-// Block represents a block in the blockchain
 type Block struct {
+	Blocknumber  int
 	Transactions []string
-	PrevBlock    *Block      // Pointer to the previous block
-	NextBlock    *Block      // Pointer to the next block
-	Root         *MerkleNode // Merkle root of the transactions in the block
+	PrevBlock    *Block
+	NextBlock    *Block
+	Root         *MerkleNode
 	Nonce        int
+	Blockhash    string
 }
 
 type MerkleNode struct {
@@ -30,6 +33,16 @@ func calculateHash(data []string) string {
 	return fmt.Sprintf("%x", hash)
 }
 
+func calculateHashWithNonce(data []string, nonce int) string {
+	combinedData := ""
+	for _, d := range data {
+		combinedData += d
+	}
+	combinedData += strconv.Itoa(nonce)
+	hash := sha256.Sum256([]byte(combinedData))
+	return fmt.Sprintf("%x", hash)
+}
+
 func buildMerkleTree(data []string) *MerkleNode {
 	if len(data) == 1 {
 		return &MerkleNode{Hashes: data, Left: nil, Right: nil}
@@ -42,7 +55,7 @@ func buildMerkleTree(data []string) *MerkleNode {
 
 func printTree(node *MerkleNode, indent string) {
 	if node != nil {
-		fmt.Println(indent+"Hash:", node.Hashes)
+		fmt.Println(indent+"Value in Merkle Node:", node.Hashes)
 	}
 	if node.Left != nil {
 		printTree(node.Left, indent+"  ")
@@ -52,75 +65,240 @@ func printTree(node *MerkleNode, indent string) {
 	}
 }
 
-// Blockchain represents a linked list of blocks
 type Blockchain struct {
-	Head *Block // Head of the blockchain (genesis block)
+	Head *Block
 }
 
-// NewBlock creates a new block with the given transactions and links it to the previous block
 func NewBlock(transactions []string, prevBlock *Block) *Block {
+
+	if prevBlock != nil {
+		block := &Block{
+			Blocknumber:  prevBlock.Blocknumber + 1,
+			Transactions: transactions,
+			PrevBlock:    prevBlock,
+			NextBlock:    nil,
+			Root:         nil,
+			Nonce:        0,
+			Blockhash:    "",
+		}
+		block.Root = buildMerkleTree(block.Transactions)
+		block.MineBlock()
+		block.CalculateBlockHash()
+		return block
+	}
+
 	block := &Block{
+		Blocknumber:  1,
 		Transactions: transactions,
 		PrevBlock:    prevBlock,
 		NextBlock:    nil,
 		Root:         nil,
 		Nonce:        0,
+		Blockhash:    "",
 	}
-
 	block.Root = buildMerkleTree(block.Transactions)
 	block.MineBlock()
+	block.CalculateBlockHash()
 	return block
+
 }
 
-func calculateHashWithNonce(data []string, nonce int) string {
-	combinedData := ""
-	for _, d := range data {
-		combinedData += d
+func (b *Block) CalculateBlockHash() {
+
+	if b.PrevBlock == nil {
+		b.Blockhash = "0"
+	} else {
+		combinedData := b.PrevBlock.Blockhash + strconv.Itoa(b.Nonce) + b.Transactions[0] + b.Transactions[1] + b.Transactions[2]
+		hash := sha256.Sum256([]byte(combinedData))
+		b.Blockhash = fmt.Sprintf("%x", hash)
 	}
-	combinedData += strconv.Itoa(nonce)
+}
+
+func (b *Block) CalculateBlockHashforverification() string {
+
+	combinedData := b.PrevBlock.Blockhash + strconv.Itoa(b.Nonce) + b.Transactions[0] + b.Transactions[1] + b.Transactions[2]
 	hash := sha256.Sum256([]byte(combinedData))
-	return fmt.Sprintf("%x", hash)
+	var newstring = fmt.Sprintf("%x", hash)
+	return newstring
+}
+
+func (b *Block) VerifyBlock() bool {
+	var current_hash = b.Blockhash
+	var hashString string
+	if b.PrevBlock == nil {
+		b.Blockhash = "0"
+		fmt.Println("The Block is verified")
+		return true
+	} else {
+		combinedData := b.PrevBlock.Blockhash + strconv.Itoa(b.Nonce) + b.Transactions[0] + b.Transactions[1] + b.Transactions[2]
+		hash := sha256.Sum256([]byte(combinedData))
+		hashString := fmt.Sprintf("%x", hash)
+		if current_hash == hashString {
+			fmt.Println("Current hash:   " + b.Blockhash)
+			fmt.Printf("Calculated hash: %s\n", hashString)
+			fmt.Println("The Block is verified")
+			return true
+		}
+
+	}
+	fmt.Println("Current hash:   " + b.Blockhash)
+	fmt.Printf(hashString)
+	return false
+}
+
+func (blockchain *Blockchain) VerifyChain() {
+	currentBlock := blockchain.Head
+	index := 0
+
+	for currentBlock != nil {
+		fmt.Printf("Verifying Block #%d:\n", index)
+		var verification = currentBlock.VerifyBlock()
+		if verification == false {
+			fmt.Println("The block number " + strconv.Itoa(index) + " seems to be tampered and can't be verified")
+			return
+		}
+		fmt.Println("------------------------")
+		index++
+		currentBlock = currentBlock.NextBlock
+	}
+	return
 }
 
 func (b *Block) MineBlock() {
-	targetPrefix := "0000" // Define the target prefix for the hash
+	targetPrefix := "0000"
 	for {
 		hash := calculateHashWithNonce(b.Transactions, b.Nonce)
 		if hash[:len(targetPrefix)] == targetPrefix {
-			// Valid nonce found
 			fmt.Printf("Block mined! Nonce: %d, Hash: %s\n", b.Nonce, hash)
 			break
 		}
-		// Increment nonce and try again
 		b.Nonce++
 	}
 }
 
-// PrintBlockchain prints the content of the blockchain
 func (bc *Blockchain) PrintBlockchain() {
 	currentBlock := bc.Head
 	for currentBlock != nil {
 		fmt.Printf("Transactions: %v\n", currentBlock.Transactions)
 		fmt.Printf("Nonce: %v\n", currentBlock.Nonce)
-		// fmt.Printf("Merkle Tree:\n")
+		fmt.Println("Block Hash Value: ", currentBlock.Blockhash)
 		printTree(currentBlock.Root, "")
 		currentBlock = currentBlock.NextBlock
 	}
 }
 
+func (blockchain *Blockchain) printBlock(x int) {
+	var currentBlock = blockchain.Head
+	for currentBlock != nil {
+		if currentBlock.Blocknumber != x {
+			currentBlock = currentBlock.NextBlock
+		}
+		if currentBlock.Blocknumber == x {
+			fmt.Println("Block number: ", x)
+			printTree(currentBlock.Root, "")
+			return
+		}
+	}
+}
+
+func (blockchain *Blockchain) changeBlock(x int) {
+	var currentBlock = blockchain.Head
+	var changestringnum int
+	var newtransaction string
+	for currentBlock != nil {
+		if currentBlock.Blocknumber != x {
+			currentBlock = currentBlock.NextBlock
+		}
+		if currentBlock.Blocknumber == x {
+			fmt.Println("Here are the transactions in this block:")
+			fmt.Printf("Transactions: %v\n", currentBlock.Transactions)
+			fmt.Println("Which transaction to change?")
+			fmt.Scanln(&changestringnum)
+			fmt.Println("Enter the new transaction")
+			fmt.Scanln(&newtransaction)
+			currentBlock.Transactions[changestringnum] = newtransaction
+			// var currentBlockHash = currentBlock.CalculateBlockHashforverification()
+			// if currentBlock.Blockhash != currentBlockHash {
+			// 	fmt.Printf("The block number", currentBlock.Blocknumber)
+			// 	fmt.Println(" has been found to be tampered with. Blockchain verification failed")
+			// 	return
+			// }
+			return
+
+		}
+	}
+}
+
+func AskInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 func main() {
-	// Create a new blockchain with the genesis block
+
+	// reader := bufio.NewReader(os.Stdin)
+	var input string
+	var input1 string
 	genesisBlock := NewBlock([]string{"Genesis Transaction1", "Genesis Transaction2", "Genesis Transaction3"}, nil)
 	blockchain := &Blockchain{Head: genesisBlock}
 
-	// Add new blocks to the blockchain
 	block2 := NewBlock([]string{"Second Transaction1", "Second Transaction2", "Second Transaction3"}, genesisBlock)
 	genesisBlock.NextBlock = block2
 
 	block3 := NewBlock([]string{"Third Transaction1", "Third Transaction2", "Third Transaction3"}, block2)
 	block2.NextBlock = block3
 
-	// Print the content of the blockchain
-	fmt.Println("Blockchain:")
-	blockchain.PrintBlockchain()
+	// 3ca38309fea3736cb0c91b4cf53048ea770bdebf35b6cbd037d532e4515bb228
+	// c1f62d9b30f2f9a6121f8e060143321869296467ebde2bde7e8645593bc70beb
+	for {
+		fmt.Println("\nChoose an option:")
+		fmt.Println("1. Print a block + Merkle Tree")
+		fmt.Println("2. Print the blockchain + Merkle trees of all blocks")
+		fmt.Println("3. Verify a specific block")
+		fmt.Println("4. Verify the blockchain")
+		fmt.Println("5. Change a block")
+		fmt.Println("0. Exit the program")
+
+		fmt.Scanln(&input)
+
+		switch input {
+		case "1":
+			fmt.Println("Choose the block number you would like to print")
+			fmt.Scanln(&input1)
+			blocknumber, err := strconv.Atoi(input1)
+			if err != nil {
+				fmt.Println("Invalid input for block number. Please enter a valid integer.")
+				continue
+			}
+			fmt.Print("Printing Block: ")
+			blockchain.printBlock(blocknumber)
+
+		case "2":
+			blockchain.PrintBlockchain()
+		case "3":
+			fmt.Print("Enter block index to verify: ")
+		case "4":
+			blockchain.VerifyChain()
+		case "5":
+			fmt.Println("Choose the block number you would like to change")
+			fmt.Scanln(&input1)
+			blocknumber, err := strconv.Atoi(input1)
+			blocknumber++
+			if err != nil {
+				fmt.Println("Invalid input for block number. Please enter a valid integer.")
+				continue
+			}
+			fmt.Print("Printing Block: ")
+			blockchain.changeBlock(blocknumber)
+
+		case "0":
+			fmt.Println("Exiting the program.")
+			return
+		default:
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
+
 }
